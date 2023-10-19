41bf6ba36d7e2bbd7c7e7f7fc9997c12250bf683
OscarDeDiegoSerrano <oscarde_diego@ceroca.cat>
2023-10-19 16:56:27 +0200

    Començament creació canvi de pantalla Game Manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,23 +5,30 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 
-        public GameObject player1;
-        public GameObject TextGameOver;
-        public GameObject Titol;
-        public GameObject BotoInici;
-        //public GameObject generadorNumeros;
-        //public GameObject generadorOperacions;
-        //public GameObject botoTornarPantallaInici;
-        public enum EstatsGameManager
-        {
-            Inici,
-            Jugant,
-            GameOver
-        }
+    public GameObject player1;
+    public GameObject TextGameOver;
+    public GameObject Titol;
+    public GameObject BotoInici;
+    public GameObject BotoControls;
+    public GameObject BotoRanking;
+    public GameObject generadorRespostes;
+    public GameObject generadorOperacions;
+    public GameObject BotoTornarInici;
+    //public GameObject generadorNumeros;
+    //public GameObject generadorOperacions;
+    //public GameObject botoTornarPantallaInici;
+    public enum EstatsGameManager
+    {
+        Inici,
+        PantallaControls,
+        PantallaRanking,
+        Jugant,
+        GameOver
+    }
 
-        private EstatsGameManager _estatGameManager;
-        // Start is called before the first frame update
-        void Start()
+    private EstatsGameManager _estatGameManager;
+    // Start is called before the first frame update
+    void Start()
     {
         _estatGameManager = EstatsGameManager.Inici;
     }
@@ -29,6 +36,33 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void ActualitzaEstatGameManager()
+    {
+        switch (_estatGameManager)
+        {
+            case EstatsGameManager.Inici:
+                player1.SetActive(false);
+                Titol.SetActive(true);
+                TextGameOver.SetActive(false);
+                BotoInici.SetActive(true);
+                BotoControls.SetActive(true);
+                BotoRanking.SetActive(true);
+                BotoTornarInici.SetActive(false);
+                break;
+
+            case EstatsGameManager.Jugant:
+                player1.SetActive(true);
+                Titol.SetActive(false);
+                TextGameOver.SetActive(false);
+                BotoInici.SetActive(false);
+                BotoControls.SetActive(false);
+                BotoRanking.SetActive(false);
+                BotoTornarInici.SetActive(false);
+                break;
+
+        }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,17 +4,43 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
+    public float _velCoche; //variable velocitat Nau
     private float velocity = 10f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _velCoche = 5f; //f de float
     }
 
     // Update is called once per frame
     void Update()
     {
-        //
+        MovimentCoche();
+    }
+
+    private void MovimentCoche()
+    {
+        //GetAxis acelera poco a poco
+        //GetAxisRaw de golpe
+        float direccioHorizontal = Input.GetAxisRaw("Horizontal");
+
+        Vector2 direccioIndicada = new Vector2(direccioHorizontal, 0);
+
+        SpriteRenderer SpriteRenderer = GetComponent<SpriteRenderer>();
+        float anchura = SpriteRenderer.bounds.size.x / 2;
+
+        //orthographicSize es la distancia desde el centro de la pantalla al borde.
+        //Camera.main.aspect devuelve cuanto mas de anchura hay respecto a la altura ya que no todas las pantallas tienen la altura = a la anchura. R = ANCHURA / ALTURA.
+        float limitEsquerraX = -Camera.main.orthographicSize * Camera.main.aspect + anchura;
+        float limitDretaX = Camera.main.orthographicSize * Camera.main.aspect - anchura;
+
+        //Ens retorna la posició actual de la nau
+        Vector2 novaPos = transform.position;
+        novaPos += direccioIndicada * _velCoche * Time.deltaTime;
+        //Time.deltaTime fa que el joc vagi en la mateixa velocitat en ordinadors diferents.
+
+        novaPos.x = Mathf.Clamp(novaPos.x, limitEsquerraX, limitDretaX);
+
+        transform.position = novaPos;
     }
 }
